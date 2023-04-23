3e3b5288daa08dcbaceece1b4f5145f91b198dd9
leila-ktb <leilaktb@mail.ubc.ca>
2023-04-23 16:55:59 -0700

    updated

--- marker_detection.py
+++ marker_detection.py
@@ -12,6 +12,7 @@ camera = 'camera2'   # camera2--jpg
 
 DIR = "./dataset/"+camera
 
+# added by leila
 ARUCO_DICT = {
     "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
     "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
