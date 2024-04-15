16313d93787da484f4a90a76c33d13e014d35019
Mohsen <azimi.eng@gmail.com>
2024-04-15 16:36:29 -0700

    updated

--- main_coTracker.py
+++ main_coTracker.py
@@ -60,7 +60,7 @@ video = torch.from_numpy(video).permute(0, 3, 1, 2)[None].float()
 
 segm_mask = np.array(Image.open(os.path.join(mask_path)))
 
-
+print(cut_strip)
 if cut_strip:
     print(f"cutting the strip: {mask_strip_y0} - {mask_strip_y1}")
     segm_mask[:mask_strip_y0] = 0
